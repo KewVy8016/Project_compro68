4f89b292b4160a5e871100ba26018e6edcf9044a
BigPeerapoln <s6806022510149@email.kmutnb.ac.th>
2025-09-12 20:12:17 +0700

    Version 6

---
+++ module/coures.py
@@ -0,0 +1,25 @@
+import def_CRUD_coures
+def main_menu():
+    """แสดงเมนูหลักและรับตัวเลือกจากผู้ใช้"""
+    while True:
+            print("\n--- เมนูจัดการรายวิชา ---")
+            print("1. เพิ่มข้อมูลรายวิชา")
+            print("2. ดูข้อมูลรายวิชาทั้งหมด")
+            print("3. แก้ไขข้อมูลรายวิชา")
+            print("4. ลบรายวิชา")
+            print("5. ย้อนกลับไปหน้าแรก")
+            choice = input("กรุณาเลือกเมนู (1-5): ")
+                
+            if choice == '1':
+                def_CRUD_coures.add_course()
+            elif choice == '2':
+                def_CRUD_coures.view_courses()
+            elif choice == '3':
+                def_CRUD_coures.update_course()
+            elif choice == '4':
+                def_CRUD_coures.delete_course()
+            elif choice == '5':
+                print("ย้อนกลับสู่เมนูหลัก...")
+                break
+            else:
+                print("ตัวเลือกไม่ถูกต้อง กรุณาลองอีกครั้ง")
